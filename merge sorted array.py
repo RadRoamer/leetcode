4e64f7c9411acf9bbbe8df151fcb8c433e2c7996
RadRoamer <zasypkovlad209@gmail.com>
2024-09-13 21:12:14 +0300

    neet code solution

--- merge sorted array.py
+++ merge sorted array.py
@@ -6,6 +6,19 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        for i in range(n):
-            nums1[-(i + 1)] = nums2[i]
-        nums1.sort()
+        last = m + n - 1
+
+    # merge in reverse order
+        while m > 0 and n > 0:
+            if nums1[m - 1] > nums2[n - 1]:
+                nums1[last] = nums1[m - 1]
+                m -= 1
+            else:
+                nums1[last] = nums2[n - 1]
+                n -= 1
+            last -= 1
+
+        # fill nums1 with leftover nums2 elements
+        while n > 0:
+            nums1[last] = nums2[n - 1]
+            n, last = n - 1, last - 1
