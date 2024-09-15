dc7715f9c6a3c8958cb58a5b594cc59a38f07d7e
RadRoamer <zasypkovlad209@gmail.com>
2024-09-15 14:34:07 +0300

    add if==main check

--- merge sorted array.py
+++ merge sorted array.py
@@ -8,7 +8,7 @@ class Solution:
         """
         last = m + n - 1
 
-    # merge in reverse order
+        # merge in reverse order
         while m > 0 and n > 0:
             if nums1[m - 1] > nums2[n - 1]:
                 nums1[last] = nums1[m - 1]
@@ -22,3 +22,13 @@ class Solution:
         while n > 0:
             nums1[last] = nums2[n - 1]
             n, last = n - 1, last - 1
+
+
+if __name__ == '__main__':
+    arr1 = [1, 2, 3, 0, 0, 0]
+    arr2 = [2, 5, 6]
+    # number of  real elements in each array
+    m = 3
+    n = 3
+    Solution().merge(arr1, m, arr2, n)
+    print(arr1)
